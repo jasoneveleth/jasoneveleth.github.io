82b425a97cd9940a25d89f85f611c7787a22a66b
jasoneveleth <jj11@eveleth.com>
2019-12-30 21:59:16 -0500

    Made the text div smaller and made a wrapper to have padding instead

--- myscript.js
+++ myscript.js
@@ -1,8 +1,8 @@
-// window.addEventListener('load', function() {
-//     document.getElementById("text").onclick = function(event) {
-//         alert("You clicked text!");
-//     };
-// });
+window.addEventListener('load', function() {
+    document.getElementById("text").onclick = function(event) {
+        alert("You clicked text!");
+    };
+});
 
 window.addEventListener('load', function() {
     document.getElementById("quote").onclick = function(event) {
