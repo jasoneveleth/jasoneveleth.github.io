8a81796244bc2cdfb91c6aaa50e66d3a2a92ed0a
Jason Eveleth <jasoneveleth@jasons-mac.lan>
2020-06-07 05:51:23 -0400

    testing mobile functionality added

--- myscript.js
+++ myscript.js
@@ -4,13 +4,12 @@ function iOS() {
     console.log("here")
     let user = navigator.userAgent;
     if (user.includes("iPhone") || user.includes("Andriod")) {
-        document.body.innerHTML = ''
-        // let bottom = document.getElementById("bottom");
-        // let top = document.getElementById("top");
-        // bottom.children[0].insertBefore(top);
-        // document.body.removeChild(document.body.children[0]);
-        // top.classList.add('item');
-        // bottom.style.flexDirection = 'column'
+        let bottom = document.getElementById("bottom");
+        let top = document.getElementById("top");
+        bottom.children[0].insertBefore(top);
+        document.body.removeChild(document.body.children[0]);
+        top.classList.add('item');
+        bottom.style.flexDirection = 'column'
     } else {
         // Do something
     }
