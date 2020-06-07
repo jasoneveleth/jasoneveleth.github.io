f04a6f56e8cb63bf3da5e77b1d52b9e3abb37174
Jason Eveleth <jasoneveleth@jasons-mac.lan>
2020-06-07 05:45:59 -0400

    testing mobile functionality

--- myscript.js
+++ myscript.js
@@ -1,5 +1,22 @@
+iOS()
+
+function iOS() {
+    console.log("here")
+    let user = navigator.userAgent;
+    if (user.includes("iPhone") || user.includes("Andriod")) {
+        let bottom = document.getElementById("bottom");
+        let top = document.getElementById("top");
+        bottom.children[0].insertBefore(top);
+        body.removeChild(body.children[0]);
+        top.classList.add('item');
+        bottom.style.flexDirection = 'column'
+    } else {
+        // Do something
+    }
+}
+
 function yee(id) {
-    let list = document.getElementsByClassName('link');
+    let list = document.getElementsByTagName('svg');
     for (let ele of list) {
         ele.style.opacity = "0.5";
     }
@@ -7,9 +24,8 @@ function yee(id) {
 }
 
 function reset() {
-    let list = document.getElementsByClassName('link');
+    let list = document.getElementsByTagName('svg');
     for (let ele of list) {
         ele.style.opacity = "1";
     }
 }
-
