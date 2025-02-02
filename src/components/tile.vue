f5b2a463c164b892ab2f7942d4c70a4b1cc1958a
Jason Eveleth <jj11@eveleth.com>
2025-02-01 21:58:36 -0500

    .

--- src/components/tile.vue
+++ src/components/tile.vue
@@ -2,7 +2,7 @@
   <div class="tile">
 	<img :src="img_src">
 	<div class="clicky">
-	  <a :href="url"><mybutton text="Url"/></a>
+	  <a target="_blank" :href="url"><mybutton text="Url"/></a>
 	  {{title}}
 	  <a :href="post"><mybutton text="Post"/></a>
 	</div>
